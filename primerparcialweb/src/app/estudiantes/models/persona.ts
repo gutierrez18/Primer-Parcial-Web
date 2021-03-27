9a1827a101a9a80fb20a4a2d89ca3328321a3b33
gutierrez18 <63273698+gutierrez18@users.noreply.github.com>
2021-03-27 12:53:44 -0500

    four commit

--- primerparcialweb/src/app/estudiantes/models/persona.ts
+++ primerparcialweb/src/app/estudiantes/models/persona.ts
@@ -4,13 +4,33 @@ export class Persona {
     primerApellido:string;
     grupoSisben:string;
     valorMatricula:number;
- 
+
     constructor(){
         this.identificacion = "";
         this.primerNombre = "";
         this.primerApellido="";
         this.grupoSisben = "";
         this.valorMatricula = 0;
+    }
+
+    calcularSubcidio(){
+      if(this.grupoSisben == "A" || this.grupoSisben == "B"){
+      this.valorMatricula = 0;
+      alert('se ha descontado el 100% de su matricula')
+      }
+
+      if(this.grupoSisben == "C"){
+        this.valorMatricula = this.valorMatricula - this.valorMatricula*0.6;
+        alert('se ha descontado el 60% de su matricula');
+
+      }
+
+      if(this.grupoSisben == "D"){
+
+        alert('no ha sido beneficiado en el descuento de matricula');
+
+      }
+
     }
 }
 
--- primerparcialweb/src/app/estudiantes/persona-registro/persona-registro.component.ts
+++ primerparcialweb/src/app/estudiantes/persona-registro/persona-registro.component.ts
@@ -9,15 +9,40 @@ import { Persona } from '../models/persona';
 })
 export class PersonaRegistroComponent implements OnInit {
   persona: Persona;
-  constructor(private personaService:PersonaService) {
-    this.persona = new  Persona(); }
+  personas: Persona[] = [];
+  constructor(private personaService: PersonaService) {
+    this.persona = new Persona();
+  }
 
   ngOnInit(): void {
-    
-    
+
+
   }
+
+
   add() {
-      this.personaService.post(this.persona);
-      alert('se ha guardado con exito');
+
+    this.personaService.get();
+    for (var i = 0; i < this.personas.length; i++) {
+      if (this.persona.identificacion == this.personas[i].identificacion) {
+        alert('este estudiante ya se encuentra registrado');
+      }
+      else {
+
+        if (this.persona.identificacion != "" && this.persona.primerNombre != "" && this.persona.primerApellido != "" && this.persona.grupoSisben != "" && this.persona.valorMatricula != 0) {
+          this.persona.calcularSubcidio();
+          this.personaService.post(this.persona);
+          alert('se ha guardado con exito');
+        }
+
+        else {
+          alert('no puede registrar campos vacios')
+        }
+
+
+      }
+    }
+
+
   }
 }
