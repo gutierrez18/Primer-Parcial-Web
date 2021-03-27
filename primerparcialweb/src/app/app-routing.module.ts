4440fcbf4712ef42cf4bf36b4a85e9c817f4ace0
gutierrez18 <63273698+gutierrez18@users.noreply.github.com>
2021-03-27 09:47:18 -0500

    second commit

--- primerparcialweb/src/app/app-routing.module.ts
+++ primerparcialweb/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonaConsultaComponent } from './estudiantes/persona-consulta/persona-consulta.component';
+import { PersonaRegistroComponent } from './estudiantes/persona-registro/persona-registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: 'personaConsulta',
+component: PersonaConsultaComponent
+
+},
+
+{
+  path: 'personaRegistro',
+  component: PersonaRegistroComponent
+  }
+
+  
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- primerparcialweb/src/app/app.module.ts
+++ primerparcialweb/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonaConsultaComponent } from './estudiantes/persona-consulta/persona-consulta.component';
+import { PersonaRegistroComponent } from './estudiantes/persona-registro/persona-registro.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaConsultaComponent,
+    PersonaRegistroComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
